e817760f2fd72cc4dcb740dd941b8e91e8a94271
y-sakaguchi-eng <y.sakaguchi.eng@gmail.com>
2024-01-28 14:44:55 +0900

    first-commit

--- pages/index.js
+++ pages/index.js
@@ -89,3 +89,13 @@ export default function Home({ posts }) {
 
 //SSGを追加
 
+export const getStaticProps = async()=> {
+  const database = await getDatabase(databaseId);
+
+  return {
+    props: {
+      posts: database,
+    },
+    revalidate:1
+  };
+};
